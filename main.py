abd8e8e22a50460800eecdf5c5913e3ef5fb9e7b
Nicolas-CARRERE <nicolascarrere.pro@gmail.com>
2024-03-17 21:30:38 +0100

    generate every url that it will be possible to call

--- main.py
+++ main.py
@@ -10,12 +10,24 @@ from time import sleep
 from db_conn import get_db_conn
 from resources.conf import Conf
 from scrap_filter import ScrapFilters
-from scrap_result import ScrapData
+from scrap_result import ScrapResults
 
 class Scrap:
     def __init__(self):
         self.conf = Conf()
 
+    def generate_results_url(self, championship, championship_filters):
+            base_url = self.conf.championship_list.get(championship)
+            query_params = {
+                "InSel": "",
+                "InCompet": championship_filters.get("InCompet"),
+                "InSpec": championship_filters.get("InSpec"),
+                "InCat": championship_filters.get("InCat")
+                # Ajoutez d'autres paramètres en fonction des filtres récupérés
+            }
+            url = base_url + "?" + "&".join([f"{key}={value}" for key, value in query_params.items()])
+            return url
+    
     def main(self):
         print("Start")
         
@@ -23,15 +35,89 @@ class Scrap:
         db_conn = get_db_conn()
         ScrapFilters.create_tables(db_conn)
         
-        scraper = Scrap()
-        for championship in scraper.conf.championship_list:
-            print("Scrapping championship: " + championship)
-            # STEP 1: Scrap Filters
-            filter = ScrapFilters(championship)
-            html = filter.fetch_data_from_url(championship)
-            filter.create_tables(db_conn)
-            filter.parse_html(self.conf.championship_list.get(championship), html, db_conn)
-            print("Scrapping championship {} done".format(championship))
+        # scraper = Scrap()
+        # for championship in scraper.conf.championship_list:
+        #     print("Scrapping championship: " + championship)
+        #     # STEP 1: Scrap Filters
+        #     filter = ScrapFilters(championship)
+        #     html = filter.fetch_data_from_url(championship)
+        #     filter.create_tables(db_conn)
+        #     filter.parse_html(self.conf.championship_list.get(championship), html, db_conn)
+        #     print("Scrapping championship {} done".format(championship))
+
+        # STEP 2: Scrap Results
+        # result_scraper = ScrapResults()
+        # Assuming db_conn is a psycopg2 connection object returned by get_db_conn()
+        db_conn = get_db_conn()
+        cursor = db_conn.cursor()
+
+        # STEP 2: Scrap Results
+        # Correction du code SQL en utilisant l'alias de table "s" au lieu de "subdomain"
+        cursor.execute("""
+            SELECT 
+                DISTINCT s.name 
+            FROM public.filter
+            JOIN public.subdomain s ON filter.subdomain_id = s.subdomain_id  
+            WHERE 1 = 1;
+        """)
+        subdomains = [sub[0] for sub in cursor.fetchall()]
+
+        cursor.execute("""
+            SELECT 
+                public.criteria_value.value 
+            FROM public.filter
+            JOIN public.criteria_value ON filter.criteria_value_id = criteria_value.criteria_value_id
+            join public.criteria c on filter.criteria_id = c.criteria_id  
+            WHERE c.name IN ('InCompet');
+        """)
+        compets = [comp[0] for comp in cursor.fetchall()]
+        
+        cursor.execute("""
+            SELECT 
+                public.criteria_value.value 
+            FROM public.filter
+            JOIN public.criteria_value ON filter.criteria_value_id = criteria_value.criteria_value_id
+            join public.criteria c on filter.criteria_id = c.criteria_id  
+            WHERE c.name IN ('InSpec');
+        """)
+        specs = [spec[0] for spec in cursor.fetchall()]
+
+        cursor.execute("""
+            SELECT 
+                public.criteria_value.value 
+            FROM public.filter
+            JOIN public.criteria_value ON filter.criteria_value_id = criteria_value.criteria_value_id
+            join public.criteria c on filter.criteria_id = c.criteria_id  
+            WHERE c.name IN ('InCat');
+        """)
+        cats = [cat[0] for cat in cursor.fetchall()]
+
+        cursor.execute("""
+            SELECT 
+                public.criteria_value.value 
+            FROM public.filter
+            JOIN public.criteria_value ON filter.criteria_value_id = criteria_value.criteria_value_id
+            join public.criteria c on filter.criteria_id = c.criteria_id  
+            WHERE c.name IN ('InPhase');
+        """)
+        phases = [phase[0] for phase in cursor.fetchall()]
+
+        # Générer toutes les combinaisons possibles des filtres
+        combinations = [(subdomain, compet, spec, cat, phase) for subdomain in subdomains for compet in compets for spec in specs for cat in cats for phase in phases]
+
+        # Créer les URLs avec les combinaisons de filtres
+        urls = []
+        for comb in combinations:
+            url = f'https://{comb[0]}.euskalpilota.fr/resultats.php?'
+            url += f'InCompet={comb[1]}&InSpec={comb[2]}&InCat={comb[3]}&InPhase={comb[4]}&InSel=&InVille=&InClub=&InDate=&InDatef=&InVoir=Voir+les+r%C3%A9sultats'
+            urls.append(url)
+
+        # Afficher les URLs générées
+        for url in urls:
+            print(url)
+
+        # db_conn.close()
+        # print("End")
 
 if __name__ == "__main__":
     scraper = Scrap()
--- scrap_result.py
+++ scrap_result.py
@@ -20,33 +20,41 @@ from requests.adapters import HTTPAdapter
 from urllib3.util import Retry
 from dotenv import load_dotenv
 from resources.conf import Conf
+from urllib.parse import urlparse
+from db_conn import get_db_conn
 
-class ScrapData:
-    def __init__(self, championship):
+# This class is used to scrape the website and the results witch are available
+class ScrapResults:
+    ''' scrap results from url'''
+    # This function __init__ is used to initialize the class
+    def __init__(self, championship: str):
         self.conf = Conf()
+        self.headers = {'User-Agent': 'Mozilla/5.0'}
+        self.session = self._create_session()
+        self.loop = asyncio.get_event_loop()
+
+    def fetch_data_from_url(self, championship):
         url = self.conf.championship_list.get(championship)
         if url:
-            headers = {'User-Agent': 'Mozilla/5.0'}
-            session = self._create_session()
-            loop = asyncio.get_event_loop()
-            loop.run_until_complete(self.fetch_data(session, url, headers))
+            return self.loop.run_until_complete(self.fetch_data(url))
         else:
-            print("Championship URL not found in the configuration.")
+            return print("Championship URL not found in the configuration.")
 
-    # This function fetch_data is used to fetch the data from the website
-    async def fetch_data(self, session, url, headers):
-        page = session.get(url, headers=headers, allow_redirects=True)
+    async def fetch_data(self, url):
+        page = self.session.get(url, headers=self.headers, allow_redirects=True)
         if page.status_code == 200:
             await asyncio.sleep(random.randint(3, 5))
-            page = session.get(url, headers=headers)
+            page = self.session.get(url, headers=self.headers)
             if page.status_code == 200:
                 html = bs4(page.text, 'html.parser')
-                selects = html.find_all('select')
+                return html
             else:
                 print("Failed to retrieve the page after delay. Status code:", page.status_code)
+                return None
         else:
             print("Failed to retrieve the initial page. Status code:", page.status_code)
-
+            return None
+        
     # This function create_session is used to create a session for the requests library.
     def _create_session(self):
         session = requests.Session()
@@ -55,5 +63,122 @@ class ScrapData:
         session.mount('https://', HTTPAdapter(max_retries=retries))
         return session
 
-if __name__ == "__main__":
-    ScrapData('basque_union')
+    # This function create_tables is used to create the tables in the database
+    @staticmethod
+    def create_tables(db_conn):
+        cursor = db_conn.cursor()
+        commands = (
+            """
+            CREATE TABLE IF NOT EXISTS subdomain (
+                subdomain_id SERIAL PRIMARY KEY,
+                name TEXT NOT NULL
+            )
+            """,
+            """
+            CREATE TABLE IF NOT EXISTS criteria (
+                criteria_id SERIAL PRIMARY KEY,
+                name TEXT NOT NULL
+            )
+            """,
+            """
+            CREATE TABLE IF NOT EXISTS criteria_value (
+                criteria_value_id SERIAL PRIMARY KEY,
+                value TEXT NULL,
+                text TEXT NULL
+            )
+            """,
+            """
+            CREATE TABLE IF NOT EXISTS data (
+                data_id SERIAL PRIMARY KEY,
+                scraped_url TEXT NULL,
+                data_html TEXT NULL,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            )
+            """,
+            """
+            CREATE TABLE IF NOT EXISTS result (
+                result_id SERIAL PRIMARY KEY,
+                subdomain_id INTEGER REFERENCES subdomain(subdomain_id),
+                criteria_id INTEGER REFERENCES criteria(criteria_id),
+                criteria_value_id INTEGER REFERENCES criteria_value(criteria_value_id)
+            )
+            """,
+            """
+            CREATE TABLE IF NOT EXISTS data_results (
+                data_id INTEGER REFERENCES data(data_id),
+                result_id INTEGER REFERENCES result(result_id)
+            );
+            """
+        )
+        for command in commands:
+            cursor.execute(command)
+        db_conn.commit()
+    
+    # This function record_exists is used to check if the record exists in the database
+    @staticmethod
+    def record_exists(db_conn, subdomain_id, criteria_id, criteria_value_id):
+        cursor = db_conn.cursor()
+        query = """
+            SELECT EXISTS(
+                SELECT 1 FROM result
+                WHERE subdomain_id = %s
+                AND criteria_id = %s
+                AND criteria_value_id = %s
+            )
+        """
+        cursor.execute(query, (subdomain_id, criteria_id, criteria_value_id))
+        return cursor.fetchone()[0]
+
+    # This function get_id is used to get the id of the record in the database
+    @staticmethod
+    def get_id(db_conn, table_name, column_name, value, text=None):
+        cursor = db_conn.cursor()
+        if table_name == "criteria_value":
+            cursor.execute(f"SELECT {table_name}_id FROM {table_name} WHERE {column_name} = %s AND text = %s", (value, text))
+        else:
+            cursor.execute(f"SELECT {table_name}_id FROM {table_name} WHERE {column_name} = %s", (value,))
+        result = cursor.fetchone()
+        if result is None:
+            if table_name == "criteria_value":
+                cursor.execute(f"INSERT INTO {table_name} ({column_name}, text) VALUES (%s, %s) RETURNING {table_name}_id", (value, text))
+            else:
+                cursor.execute(f"INSERT INTO {table_name} ({column_name}) VALUES (%s) RETURNING {table_name}_id", (value,))
+            result = cursor.fetchone()
+            db_conn.commit()
+        return result[0]
+
+    # This function parse_html is used to parse the html and save the results to the database
+    @staticmethod
+    def parse_html(url, html, db_conn):
+        parsed_url = urlparse(url)
+        subdomain = parsed_url.hostname.split('.')[0]
+        soup = html
+        selects = soup.find_all('select')
+        for select in selects:
+            options = select.find_all('option')
+            for option in options:
+                subdomain_id = ScrapResults.get_id(db_conn, "subdomain", "name", subdomain)
+                criteria_id = ScrapResults.get_id(db_conn, "criteria", "name", select['name'])
+                criteria_value_id = ScrapResults.get_id(db_conn, "criteria_value", "value", option['value'], option.text)
+                if not ScrapResults.record_exists(db_conn, subdomain_id, criteria_id, criteria_value_id):
+                    ScrapResults.save_to_db(db_conn, subdomain, select['name'], option['value'], option.text)
+
+    # This function save_to_db is used to save the record to the database
+    @staticmethod
+    def save_to_db(db_conn, subdomain, criteria, criteria_value, option_text):
+        cursor = db_conn.cursor()
+
+        subdomain_id = ScrapResults.get_id(db_conn, "subdomain", "name", subdomain)
+        criteria_id = ScrapResults.get_id(db_conn, "criteria", "name", criteria)
+        criteria_value_id = ScrapResults.get_id(db_conn, "criteria_value", "value", criteria_value, option_text if option_text else '')
+
+        cursor.execute("SELECT * FROM criteria_value WHERE criteria_value_id = %s", (criteria_value_id,))
+        result = cursor.fetchone()
+        if result is None:
+            raise ValueError(f"criteria_value_id {criteria_value_id} does not exist in criteria_value table")
+        else:
+            if not ScrapResults.record_exists(db_conn, subdomain_id, criteria_id, criteria_value_id):
+                cursor.execute("INSERT INTO result (subdomain_id, criteria_id, criteria_value_id) VALUES (%s, %s, %s)",
+                            (subdomain_id, criteria_id, criteria_value_id))
+                db_conn.commit()
