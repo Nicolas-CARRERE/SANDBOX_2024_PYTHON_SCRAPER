ac840e06e1ed77f6739a87cb57ea24b19a963dca
root <root@PC-FIXE-NICO>
2024-04-07 17:05:28 +0200

    can now update a record by adding the score or the comment

--- scrap_result.py
+++ scrap_result.py
@@ -188,6 +188,44 @@ class ScrapResults:
     @staticmethod
     def save_into_db(db_conn, scraped_url, title, championship, date, game, team1, playerA1, playerB1, team2, playerA2, playerB2, score, comment, created_at, updated_at):
         cursor = db_conn.cursor()
+        # firstly try to update the record
+        try:
+            query = """
+                UPDATE data
+                SET
+                    scraped_url = %s,
+                    title = %s,
+                    championship = %s,
+                    date = %s,
+                    game = %s,
+                    team1 = %s,
+                    playerA1 = %s,
+                    playerB1 = %s,
+                    team2 = %s,
+                    playerA2 = %s,
+                    playerB2 = %s,
+                    score = %s,
+                    comment = %s,
+                    updated_at = %s
+                WHERE scraped_url = %s
+                AND title = %s
+                AND championship = %s
+                AND date = %s
+                AND game = %s
+                AND team1 = %s
+                AND playerA1 = %s
+                AND playerB1 = %s
+                AND team2 = %s
+                AND playerA2 = %s
+                AND playerB2 = %s
+            """
+            cursor.execute(query, (scraped_url, title, championship, date, game, team1, playerA1, playerB1, team2, playerA2, playerB2, score, comment, updated_at, scraped_url, title, championship, date, game, team1, playerA1, playerB1, team2, playerA2, playerB2))
+            db_conn.commit()
+            return cursor.lastrowid
+        except Exception as e:
+            print(e)
+            pass
+            
         query = """
             INSERT INTO data (scraped_url, title, championship, date, game, team1, playerA1, playerB1, team2, playerA2, playerB2, score, comment, created_at, updated_at)
             VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
